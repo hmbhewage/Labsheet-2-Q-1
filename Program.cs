fcad66d42c11b9877475fb60771c9498bca4afae
Administrator <Administrator@MERAJ-BINOVIN-H>
2024-02-28 15:11:49 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Book
+{
+    public string Title { get; set; }
+    public string Author { get; set; }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Create a Book object
+        Book myBook = new Book();
+
+        // Set values for the properties
+        myBook.Title = "The Lord of the Rings";
+        myBook.Author = "J.R.R. Tolkien";
+
+        // Display information on console
+        Console.WriteLine($"Title: {myBook.Title}");
+        Console.WriteLine($"Author: {myBook.Author}");
+    }
+}
